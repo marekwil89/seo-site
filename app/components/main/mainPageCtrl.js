d91a5d2a77ca5845d5e68c73cf6e59d7f6bc3497
marekwil89 <marekwil89@gmail.com>
2017-03-26 23:17:06 +0200

    V 0.4

--- app/components/main/mainPageCtrl.js
+++ app/components/main/mainPageCtrl.js
@@ -1,2 +1,23 @@
 angular.module("mainPageModule", []).controller('mainPageCtrl', function($scope){
+
+
+  $scope.resizePanel = function(event){
+    var currentElem = angular.element(event.currentTarget)
+
+
+    if(currentElem.parent().hasClass('resize')){
+      currentElem.removeClass('open')
+      currentElem.prev('.panel-info').removeClass('show');
+      return currentElem.parent().removeClass('resize')
+    }
+    currentElem.parent().siblings().removeClass('resize');
+    currentElem.addClass('open')
+    currentElem.prev('.panel-info').addClass('show');
+    currentElem.parent().addClass('resize');
+
+    console.log(angular.element(event.currentTarget).parent().hasClass('resize'))
+  }
+
+
+
 })
