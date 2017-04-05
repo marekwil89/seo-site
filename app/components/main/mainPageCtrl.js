ca9eeee12939b033c5f8d2dd16420ccecdcf0ea8
marekwil89 <marekwil89@gmail.com>
2017-04-05 10:59:09 +0200

    Update mainPageCtrl.js

--- app/components/main/mainPageCtrl.js
+++ app/components/main/mainPageCtrl.js
@@ -1,9 +1,9 @@
-angular.module("mainPageModule", []).controller('mainPageCtrl', function($scope){
+angular.module("mainPageModule", [])
 
+.controller('mainPageCtrl', function($scope, anchorSmoothScroll, $location){
 
   $scope.resizePanel = function(event){
     var currentElem = angular.element(event.currentTarget)
-
 
     if(currentElem.parent().hasClass('resize')){
       currentElem.removeClass('open')
@@ -14,10 +14,56 @@ angular.module("mainPageModule", []).controller('mainPageCtrl', function($scope)
     currentElem.addClass('open')
     currentElem.prev('.panel-info').addClass('show');
     currentElem.parent().addClass('resize');
-
-    console.log(angular.element(event.currentTarget).parent().hasClass('resize'))
   }
 
-
-
+  $scope.gotoElement = function (eID){
+    $location.hash('bottom');
+    anchorSmoothScroll.scrollTo(eID);
+  };
 })
+
+.service('anchorSmoothScroll', function(){
+    
+   this.scrollTo = function(eID) {
+        
+      var startY = currentYPosition();
+      var stopY = elmYPosition(eID);
+      var distance = stopY > startY ? stopY - startY : startY - stopY;
+      if (distance < 100) {
+          scrollTo(0, stopY); return;
+      }
+      var speed = Math.round(distance / 100);
+      if (speed >= 20) speed = 20;
+      var step = Math.round(distance / 25);
+      var leapY = stopY > startY ? startY + step : startY - step;
+      var timer = 0;
+      if (stopY > startY) {
+          for ( var i=startY; i<stopY; i+=step ) {
+              setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+              leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+          } return;
+      }
+      for ( var i=startY; i>stopY; i-=step ) {
+          setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+          leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+      }
+
+      function currentYPosition() {
+          if (self.pageYOffset) return self.pageYOffset;
+          if (document.documentElement && document.documentElement.scrollTop)
+              return document.documentElement.scrollTop;
+          if (document.body.scrollTop) return document.body.scrollTop;
+          return 0;
+      }
+
+      function elmYPosition(eID) {
+          var elm = document.getElementById(eID);
+          var y = elm.offsetTop;
+          var node = elm;
+          while (node.offsetParent && node.offsetParent != document.body) {
+              node = node.offsetParent;
+              y += node.offsetTop;
+          } return y;
+      }
+    };  
+});
